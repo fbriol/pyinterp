e0827ca5f85a957245d0cbe3585ac9ca4829fabb
Frédéric BRIOL <fbriol@gmail.com>
2025-12-09 10:39:30 +0100

    feat(rtree): Implement Query class with configuration methods for boundary check, k value, number of threads, and radius feat(type hints): Add type hints for NDArray2D and NDArray1D types feat(init): Update imports in __init__.py for new type hints and geodetic module

--- pyinterp/type_hints.py
+++ pyinterp/type_hints.py
@@ -1,4 +1,6 @@
-from typing import TYPE_CHECKING, Any
+"""Type hints for pyinterp package."""
+
+from typing import TYPE_CHECKING, Any, Literal
 
 if TYPE_CHECKING:
     import numpy as np
@@ -8,6 +10,9 @@ if TYPE_CHECKING:
     ThreeDims = tuple[int, int, int]
     FourDims = tuple[int, int, int, int]
 
+    Vector3Float32 = np.ndarray[tuple[Literal[3]], np.dtype[np.float32]]
+    Vector3Float64 = np.ndarray[tuple[Literal[3]], np.dtype[np.float64]]
+
     NDArray1D = np.ndarray[OneDim, np.dtype[Any]]
     NDArray1DBool = np.ndarray[OneDim, np.dtype[np.int8]]
     NDArray1DDateTime = np.ndarray[OneDim, np.dtype[np.datetime64]]
@@ -16,5 +21,8 @@ if TYPE_CHECKING:
     NDArray1DFloat64 = np.ndarray[OneDim, np.dtype[np.float64]]
     NDArray1DInt64 = np.ndarray[OneDim, np.dtype[np.int64]]
     NDArray1DTimeDelta64 = np.ndarray[OneDim, np.dtype[np.timedelta64]]
+    NDArray1DUInt32 = np.ndarray[OneDim, np.dtype[np.uint32]]
 
+    NDArray2DFloat32 = np.ndarray[TwoDims, np.dtype[np.float32]]
+    NDArray2DFloat64 = np.ndarray[TwoDims, np.dtype[np.float64]]
     NDArray2DInt64 = np.ndarray[TwoDims, np.dtype[np.int64]]
