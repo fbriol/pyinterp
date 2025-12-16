dc61a90083ac3f1826bcf0be95657925acc04bac
Frédéric BRIOL <fbriol@gmail.com>
2025-12-16 18:07:52 +0100

    refactor(linestring): update LineString to use cartesian coordinates and adjust namespace

--- cxx/include/pyinterp/geometry/cartesian/linestring.hpp
+++ cxx/include/pyinterp/geometry/cartesian/linestring.hpp
@@ -1,25 +1,25 @@
 #pragma once
 
-#include "pyinterp/geometry/geographic/point.hpp"
+#include "pyinterp/geometry/cartesian/point.hpp"
 #include "pyinterp/geometry/linestring.hpp"
 
-namespace pyinterp::geometry::geographic {
+namespace pyinterp::geometry::cartesian {
 
-/// @brief Type representing a linestring in geodetic coordinates
+/// @brief Type representing a linestring in cartesian coordinates
 using LineString = pyinterp::geometry::LineString<Point>;
 
-}  // namespace pyinterp::geometry::geographic
+}  // namespace pyinterp::geometry::cartesian
 
 namespace boost::geometry::traits {
 
 template <>
-struct tag<pyinterp::geometry::geographic::LineString> {
+struct tag<pyinterp::geometry::cartesian::LineString> {
   using type = linestring_tag;
 };
 
 template <>
-struct point_type<pyinterp::geometry::geographic::LineString> {
-  using type = pyinterp::geometry::geographic::Point;
+struct point_type<pyinterp::geometry::cartesian::LineString> {
+  using type = pyinterp::geometry::cartesian::Point;
 };
 
 }  // namespace boost::geometry::traits
