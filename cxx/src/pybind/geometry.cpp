8b6f7111c4d3e0af09c1e6e716fcf19f52814531
Frédéric BRIOL <fbriol@gmail.com>
2025-12-17 19:14:43 +0100

    feat(geometry): add init_cartesian function to initialize Cartesian calculations module

--- cxx/src/pybind/geometry.cpp
+++ cxx/src/pybind/geometry.cpp
@@ -1,12 +1,14 @@
 #include "pyinterp/pybind/geometry.hpp"
 
 #include "pyinterp/pybind/geometry/geographic.hpp"
+#include "pyinterp/pybind/geometry/cartesian.hpp"
 
 namespace pyinterp::pybind {
 
 void init_geometry(nanobind::module_& m) {
   auto geometry = m.def_submodule("geometry", "Geometry module");
   pyinterp::geometry::pybind::init_geographic(geometry);
+  pyinterp::geometry::pybind::init_cartesian(geometry);
 }
 
 }  // namespace pyinterp::pybind
