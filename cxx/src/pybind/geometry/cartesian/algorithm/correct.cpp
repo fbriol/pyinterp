772849907384cfcbfa276ee941b57bc22e4faa6d
Frédéric BRIOL <fbriol@gmail.com>
2025-12-19 05:53:46 +0100

    feat(geometry): implement correct algorithm for Cartesian and Geographic geometries

---
+++ cxx/src/pybind/geometry/cartesian/algorithm/correct.cpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2025 CNES
+//
+// All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+#include "pyinterp/pybind/geometry/algorithms/correct.hpp"
+
+#include <nanobind/nanobind.h>
+
+#include <boost/geometry.hpp>
+
+#include "pyinterp/pybind/geometry/algorithm_binding_helpers.hpp"
+
+namespace nb = nanobind;
+using pyinterp::geometry::pybind::GeometryNamespace;
+
+namespace pyinterp::geometry::cartesian::pybind {
+
+auto init_correct(nb::module_& m) -> void {
+  geometry::pybind::init_correct<GeometryNamespace::kCartesian>(m);
+}
+
+}  // namespace pyinterp::geometry::cartesian::pybind
