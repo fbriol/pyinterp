d17cf9c7f43020d9db541ed96bde774393fdc000
Frédéric BRIOL <fbriol@gmail.com>
2025-12-19 05:59:49 +0100

    feat(geometry): implement difference algorithm for Cartesian and Geographic geometries

---
+++ cxx/src/pybind/geometry/cartesian/algorithm/difference.cpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2025 CNES
+//
+// All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+#include "pyinterp/pybind/geometry/algorithms/difference.hpp"
+
+#include <nanobind/nanobind.h>
+
+#include <boost/geometry.hpp>
+
+#include "pyinterp/pybind/geometry/algorithm_binding_helpers.hpp"
+
+namespace nb = nanobind;
+using pyinterp::geometry::pybind::GeometryNamespace;
+
+namespace pyinterp::geometry::cartesian::pybind {
+
+auto init_difference(nb::module_& m) -> void {
+  geometry::pybind::init_difference<GeometryNamespace::kCartesian>(m);
+}
+
+}  // namespace pyinterp::geometry::cartesian::pybind
