47bb44242282db3323a4d75503d86614edb6bc4c
Frédéric BRIOL <fbriol@gmail.com>
2025-12-19 07:49:50 +0100

    feat(geometry): implement convex hull algorithms for Cartesian and Geographic geometries

--- cxx/src/pybind/geometry/cartesian/algorithm/convex_hull.cpp
+++ cxx/src/pybind/geometry/cartesian/algorithm/convex_hull.cpp
@@ -2,22 +2,42 @@
 //
 // All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-#include "pyinterp/pybind/geometry/algorithms/convex_hull.hpp"
-
 #include <nanobind/nanobind.h>
 
 #include <boost/geometry.hpp>
 
+#include "pyinterp/geometry/cartesian/polygon.hpp"
 #include "pyinterp/pybind/geometry/algorithm_binding_helpers.hpp"
 
 namespace nb = nanobind;
 using nb::literals::operator""_a;
-using pyinterp::geometry::pybind::GeometryNamespace;
 
 namespace pyinterp::geometry::cartesian::pybind {
 
+constexpr auto kConvexHullDoc = R"doc(
+Calculates the convex hull of a geometry.
+
+The convex hull is the smallest convex geometry that contains all points
+of the input geometry. It is always returned as a Polygon.
+
+Args:
+    geometry: Geometric object to compute convex hull for.
+
+Returns:
+    A Polygon representing the convex hull.
+)doc";
+
 auto init_convex_hull(nb::module_& m) -> void {
-  geometry::pybind::init_convex_hull<GeometryNamespace::kCartesian>(m);
+  auto convex_hull_impl = [](const auto& g) -> Polygon {
+    nanobind::gil_scoped_release release;
+    Polygon result;
+    boost::geometry::convex_hull(g, result);
+    return result;
+  };
+  geometry::pybind::define_for_geometries<decltype(convex_hull_impl),
+                                          GEOMETRY_TYPES(cartesian)>(
+      m, "convex_hull", kConvexHullDoc, std::move(convex_hull_impl));
+  ;
 }
 
 }  // namespace pyinterp::geometry::cartesian::pybind
--- cxx/src/pybind/geometry/geographic/algorithm/convex_hull.cpp
+++ cxx/src/pybind/geometry/geographic/algorithm/convex_hull.cpp
@@ -2,12 +2,15 @@
 //
 // All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-#include "pyinterp/pybind/geometry/algorithms/convex_hull.hpp"
+#include "pyinterp/geometry/geographic/algorithms/convex_hull.hpp"
 
 #include <nanobind/nanobind.h>
 
 #include <boost/geometry.hpp>
 
+#include "pyinterp/geometry/geographic/algorithms/strategy.hpp"
+#include "pyinterp/geometry/geographic/polygon.hpp"
+#include "pyinterp/geometry/geographic/spheroid.hpp"
 #include "pyinterp/pybind/geometry/algorithm_binding_helpers.hpp"
 
 namespace nb = nanobind;
@@ -16,8 +19,40 @@ using pyinterp::geometry::pybind::GeometryNamespace;
 
 namespace pyinterp::geometry::geographic::pybind {
 
+constexpr auto kConvexHullDoc = R"doc(
+Calculates the convex hull of a geometry.
+
+The convex hull is the smallest convex geometry that contains all points
+of the input geometry. It is always returned as a Polygon.
+
+Args:
+    geometry: Geometric object.
+    spheroid: Optional spheroid for geodetic calculations. If not provided, uses
+        WGS84 ellipsoid.
+    strategy: Calculation strategy.
+
+Returns:
+    A Polygon representing the convex hull.
+)doc";
+
 auto init_convex_hull(nb::module_& m) -> void {
-  geometry::pybind::init_convex_hull<GeometryNamespace::kGeographic>(m);
+  auto convex_hull_impl = [](const auto& geometry,
+                             const std::optional<Spheroid>& spheroid,
+                             StrategyMethod strategy) -> Polygon {
+    nb::gil_scoped_release release;
+    return convex_hull(geometry, spheroid, strategy);
+  };
+
+  ([&]<typename... Geometry>() {
+    (..., m.def(
+              "area",
+              [=](const Geometry& g, const std::optional<Spheroid>& wgs,
+                  StrategyMethod strategy) -> Polygon {
+                return convex_hull_impl(g, wgs, strategy);
+              },
+              "geometry"_a, nb::kw_only(), "spheroid"_a = std::nullopt,
+              "strategy"_a = StrategyMethod::kVincenty, kConvexHullDoc));
+  }).template operator()<GEOMETRY_TYPES(geographic)>();
 }
 
 }  // namespace pyinterp::geometry::geographic::pybind
