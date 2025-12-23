890b7bdc95743b2533bd73fb55e85a4e029b5d0e
Frédéric BRIOL <fbriol@gmail.com>
2025-12-23 07:31:53 +0100

    feat(fill): add bindings for FFT in-painting, Gauss-Seidel, and Multigrid methods

--- cxx/src/pybind/fill.cpp
+++ cxx/src/pybind/fill.cpp
@@ -1,6 +1,9 @@
 #include "pyinterp/pybind/fill.hpp"
 
+#include "pyinterp/pybind/fill/fft_inpaint.hpp"
+#include "pyinterp/pybind/fill/gauss_seidel.hpp"
 #include "pyinterp/pybind/fill/loess.hpp"
+#include "pyinterp/pybind/fill/multigrid.hpp"
 
 namespace nb = nanobind;
 
@@ -9,7 +12,10 @@ namespace pyinterp::pybind {
 auto init_fill(nb::module_& m) -> void {
   auto fill = m.def_submodule("fill", "Methods to fill missing data.");
 
+  fill::pybind::bind_fft_inpaint(fill);
+  fill::pybind::bind_gauss_seidel(fill);
   fill::pybind::bind_loess(fill);
+  fill::pybind::bind_multigrid(fill);
 }
 
 }  // namespace pyinterp::pybind
