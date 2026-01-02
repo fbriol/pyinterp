17f3ddee035f22fc0fcafcb7faeeb2f877b85cb8
Frédéric BRIOL <fbriol@gmail.com>
2026-01-02 21:04:26 +0100

    style: format code for improved readability in period module

--- pyinterp/core/__init__.pyi
+++ pyinterp/core/__init__.pyi
@@ -16,7 +16,7 @@ from ..type_hints import (
     TwoDims,
     OneDim,
 )
-from . import geometry, fill,  period
+from . import geometry, fill, period
 from .config import geometric, rtree, windowed
 
 # Type alias for temporal coordinate arrays
