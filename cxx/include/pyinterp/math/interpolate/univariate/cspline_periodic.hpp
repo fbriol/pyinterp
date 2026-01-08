52bbca391611345031adaa634968b1599772094f
Frédéric BRIOL <fbriol@gmail.com>
2026-01-08 18:43:34 +0100

    refactor: update minimum size requirement for cubic spline interpolation to 4

--- cxx/include/pyinterp/math/interpolate/univariate/cspline_periodic.hpp
+++ cxx/include/pyinterp/math/interpolate/univariate/cspline_periodic.hpp
@@ -23,7 +23,7 @@ class CSplinePeriodic : public CSplineBase<T> {
   /// @brief Returns the minimum number of points required for the
   /// interpolation.
   /// @return Minimum number of points
-  [[nodiscard]] constexpr auto min_size() const -> int64_t final { return 2; }
+  [[nodiscard]] constexpr auto min_size() const -> int64_t final { return 4; }
 
  private:
   /// @brief Compute the coefficients of the interpolation
